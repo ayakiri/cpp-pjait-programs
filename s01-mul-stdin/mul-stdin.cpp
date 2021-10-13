731c4a7f526d6028c13eff50fe15077ca98bd94f
s24305 <s24305@pjwstk.edu.pl>
2021-10-13 14:33:57 +0200

    zadanie 4 - wyklad 05.10.2021, add-stdin, sub-stdin, mul-stdin, div-stdin

---
+++ s01-mul-stdin/mul-stdin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+auto zapytaj_o_liczbe(std::string prompt) -> int {
+    std::cout << prompt << "(integer) ";
+    auto liczba = std::string{};
+    std::getline(std::cin, liczba);
+    return std::stoi(liczba);
+}
+
+auto main(int argc, char* argv[]) -> int {
+    auto const a = zapytaj_o_liczbe("a = ");
+    auto const b = zapytaj_o_liczbe("b = ");
+    std::cout << (a * b) << "\n";
+    return 0;
+}
