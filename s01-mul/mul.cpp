ff31f41b736d127d3dbddbc53e5b7107241b1332
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2021-10-14 07:37:35 +0000

    Delete mul.cpp

--- s01-mul/mul.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-auto main(int argc, char* argv[]) -> int {
-    auto const a = std::stof(argv[1]);
-    auto const b = std::stof(argv[2]);
-    std::cout << (a * b) << "\n";
-    return 0;
-}
