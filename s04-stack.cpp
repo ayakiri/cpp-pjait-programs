1694e6af3a8f6e93cbe5bb7908fdfebe9b5ef2ae
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2021-11-10 16:58:16 +0000

    Zadanie 1 (10.11.2021) - stack

---
+++ s04-stack.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stack>
+
+auto main(int argc, char* argv[]) -> int {
+    // sprawdz czy jest tylko jeden argument    
+    if(argc > 2){
+        std::cerr << "Input only ONE string";
+        return 0;
+    }
+
+    std::stack<char> stackOfBrackets;
+    std::string givenBrackets = argv[1];
+
+    // kolejno przechodz po znakach w stringu givenBrackets
+    for(int i = 0; i < givenBrackets.length(); i++){
+
+        //jesli nawias otwierajacy - dorzuc go na gore stosu
+        if (givenBrackets[i] == '(' || givenBrackets[i] == '{' || givenBrackets[i] == '['){
+            stackOfBrackets.push(givenBrackets[i]);
+        }
+
+        //jesli nawias zamykajacy - sprzawdz co na gorze stosu
+        else if (givenBrackets[i] == ')' || givenBrackets[i] == '}' || givenBrackets[i] == ']'){
+
+            //sprawdz ktory to nawias i wedlug tego sprawdz czy "zamyka sie" z nawiasem z topu stosu
+            // jesli tak - usun nawias otwierajacy z topu stosu
+            // jesli nie - nawiasy sa bledne, czyli "ERROR"
+            switch (givenBrackets[i]){
+                case ')':
+                    if(stackOfBrackets.top() == '('){
+                        stackOfBrackets.pop();
+                    } else {
+                        std::cout << "ERROR";
+                        return 0;
+                    }
+                    break;
+                case '}':
+                    if(stackOfBrackets.top() == '{'){
+                        stackOfBrackets.pop();
+                    } else {
+                        std::cout << "ERROR";
+                        return 0;
+                    }
+                    break;
+                case ']':
+                    if(stackOfBrackets.top() == '['){
+                        stackOfBrackets.pop();
+                    } else {
+                        std::cout << "ERROR";
+                        return 0;
+                    }
+                    break;
+            }
+        }
+        //jesli podano nie nawias
+        else {
+            std::cerr << "Input ONLY brackets in your string";
+        }
+    }
+
+    std::cout << "OK";
+
+    return 0;
+}
